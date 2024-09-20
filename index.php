b8115b0fd95bdb6fd06fcde18093829bf512efa9
clarisse-lebaut <clarisse.le-baut@laplateforme.io>
2024-09-20 20:17:45 +0200

    get the trails data + link

--- index.php
+++ index.php
@@ -35,20 +35,22 @@ $trails = getAllTrails($connectBDD);
 
         <section>
             <h2>Overflow</h2>
+            <p>pour rendre cliquable il faut d'abord mettre chaque sentier dans un card</p>
                 <?php foreach ($trails as $trail): ?>
-                    <p><?php echo htmlspecialchars($trail['trail_id']); ?></p>
-                    <p><?php echo htmlspecialchars($trail['name']); ?></p>
-                    <p><?php echo htmlspecialchars($trail['length_km']); ?></p>
-                    <p><?php echo htmlspecialchars($trail['time']); ?></p>
-                    <p><?php echo htmlspecialchars($trail['description']); ?></p>
-                    <p><?php echo htmlspecialchars($trail['status']); ?></p>
-                    <img src="<?php echo ($trail['image']); ?>" alt="<?php echo($trail['name']); ?>" width="200">
-                    <p><?php echo htmlspecialchars($trail['infos']); ?></p>
-                    <p><?php echo htmlspecialchars($trail['acces']); ?></p>
-                
+                    <div class="card">
+                        <a href="">
+                            <p><?php echo htmlspecialchars($trail['name']); ?></p>
+                            <img src="<?php echo ($trail['image']); ?>" alt="<?php echo($trail['name']); ?>" width="200">
+                        </a>
+                            <p><?php echo htmlspecialchars($trail['length_km']); ?></p>
+                            <p><?php echo htmlspecialchars($trail['time']); ?></p>
+                        </div>
+                    <!-- <p><?php echo htmlspecialchars($trail['trail_id']); ?></p> -->
+                    <!-- <p><?php echo htmlspecialchars($trail['description']); ?></p>
+                    <p><?php echo htmlspecialchars($trail['status']); ?></p> -->
+                    <!-- <p><?php echo htmlspecialchars($trail['infos']); ?></p>
+                    <p><?php echo htmlspecialchars($trail['acces']); ?></p>  -->
                 <?php endforeach; ?>
-            <p>Grid de tous les sentiers récupérer dans la base de données</p>
-            <p>il faut rendre chaque item cliquable pour acceder à la page détails d'un seul sentier</p>
         </section>
     </main>
     <footer></footer>
