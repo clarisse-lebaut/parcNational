e71d66b92eb5b2dbf8d5103e13412d906dbf8f05
clarisse-lebaut <clarisse.le-baut@laplateforme.io>
2024-09-20 18:33:40 +0200

    start the main page for trails : description tasks

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# parcNational
+# 🏞️ Parc National des Calanques de Marseille
+
+Sur cette branche : la page permettant de voir tous les sentiers.
+
+Détails de la page : 
+- le titre
+- le filtre des sentiers (trier par caractéristique)
+- grid des sentiers cliquable pour avoir le détails
---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trails</title>
+</head>
+<body>
+    <header></header>
+    <main>
+        <h1>Titre : Les Sentiers</h1>
+
+        <section>
+            <h2>Présentation de la page</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur, quidem fugiat dolorum illum doloremque autem!</p>
+        </section>
+
+        <section>
+            <h2>Filtre</h2>
+            <p>Faire la fonction pour le filtre en JavaScript</p>
+        </section>
+
+        <section>
+            <h2>Overflow</h2>
+            <p>Grid de tous les sentiers récupérer dans la base de données</p>
+        </section>
+    </main>
+    <footer></footer>
+</body>
+</html>
