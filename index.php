e2858b52aed610f3fc211fa46cb9d546e58b8c5a
clarisse-lebaut <clarisse.le-baut@laplateforme.io>
2024-09-21 19:49:16 +0200

    add icon + foreach for card

--- index.php
+++ index.php
@@ -25,26 +25,37 @@ $trails = getAllTrails($connectBDD);
 
         <section>
             <h2>Présentation de la page</h2>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur, quidem fugiat dolorum illum doloremque autem!</p>
+            <p>
+                Site naturel remarquable, le Parc national des Calanques – situé entre Marseille et Cassis – regorge d’endroits 
+                d’exception. Arpenter les chemins est le meilleur moyen de découvrir les multiples facettes de ce parc national. 
+                De nombreux chemins balisés sont accessibles pour les visiteurs en quête de randonnées. Quel que soit le niveau 
+                de difficulté choisi, chaque itinéraire donne lieu à des paysages fabuleux entre mer et montagne.
+            </p>
         </section>
 
         <section>
             <h2>Filtre</h2>
-            <p>Faire la fonction pour le filtre en JavaScript</p>
+            <p>Insérer la fonction pour le filtre en JavaScript</p>
         </section>
 
         <section>
             <h2>Overflow</h2>
-            <p>pour rendre cliquable il faut d'abord mettre chaque sentier dans un card</p>
                 <?php foreach ($trails as $trail): ?>
                     <div class="card">
-                        <a href="">
-                            <p><?php echo htmlspecialchars($trail['name']); ?></p>
-                            <img src="<?php echo ($trail['image']); ?>" alt="<?php echo($trail['name']); ?>" width="200">
-                        </a>
+                        <div class="card_top">
+                            <a href="">
+                                <p><?php echo htmlspecialchars($trail['name']); ?></p>
+                                <img src="<?php echo ($trail['image']); ?>" alt="<?php echo($trail['name']); ?>" width="200">
+                            </a>
+                        </div>
+                        <div class="card_details">
                             <p><?php echo htmlspecialchars($trail['length_km']); ?></p>
                             <p><?php echo htmlspecialchars($trail['time']); ?></p>
                         </div>
+                        <!-- mettre des icônes -->
+                        <button><img src="./assets/icon/favorite-fill.svg" alt="heart icon">Ajouter au favoris</button>
+                        <button><img src="./assets/icon/hiking.svg" alt="">Ajouter à mes kilomètres</button>
+                    </div>
                     <!-- <p><?php echo htmlspecialchars($trail['trail_id']); ?></p> -->
                     <!-- <p><?php echo htmlspecialchars($trail['description']); ?></p>
                     <p><?php echo htmlspecialchars($trail['status']); ?></p> -->
