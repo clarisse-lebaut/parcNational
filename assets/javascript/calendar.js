d8478d5ceb44d91516a6e9565183b7dd2511dad0
maily-vannobel <maily.vannobel@laplateforme.io>
2024-09-25 14:12:30 +0200

    add plugin rrule for yearly reccurence of campsite's holidays

--- assets/javascript/calendar.js
+++ assets/javascript/calendar.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     let calendarEl = document.getElementById('calendar');
 
+    // Récupération de l'ID du camping depuis l'URL
     const urlParams = new URLSearchParams(window.location.search);
     const campsite_id = urlParams.get('campsite_id');
 
@@ -15,64 +16,56 @@ document.addEventListener('DOMContentLoaded', function() {
             end: 'prev,next today'
         },
 
-        events: function(fetchInfo, successCallback, failureCallback) {
+        events: function(successCallback) {
             let events = [];
+            const defaultColor = '#FF6347'; 
+
+            // plugin rrule : regle de récurrence
+            const generateRRuleEvent = (title, start) => {
+                return {
+                    title: title,
+                    color: defaultColor, 
+                    allDay: true,
+                    rendering: 'background', 
+                    rrule: {
+                        freq: 'yearly', 
+                        dtstart: start, 
+                        until: '2040-12-31' 
+                    }
+                };
+            };
 
             if (campsite_id == 1) {
-                events = [
-                    {
-                        title: 'Fermeture - Camping Les Cigales',
-                        start: '2024-03-25',
-                        end: '2024-11-06',
-                        color: '#FF6347',
-                        allDay: true
-                    }
-                ];
+                events.push(generateRRuleEvent(
+                    'Fermeture - Camping Les Cigales',
+                    '2024-03-25'
+                ));
             } else if (campsite_id == 2) {
-                events = [
-                    {
-                        title: 'Fermeture - Camping de Ceyreste',
-                        start: '2024-04-15',
-                        end: '2024-10-01',
-                        color: '#FF6377',
-                        allDay: true
-                    }
-                ];
+                events.push(generateRRuleEvent(
+                    'Fermeture - Camping de Ceyreste',
+                    '2024-04-15'
+                ));
             } else if (campsite_id == 3) {
-                events = [
-                    {
-                        title: 'Fermeture - Camping La Baie des Anges',
-                        start: '2024-03-29',
-                        end: '2024-09-30',
-                        color: '#32CD32',
-                        allDay: true
-                    }
-                ];
+                events.push(generateRRuleEvent(
+                    'Fermeture - Camping La Baie des Anges',
+                    '2024-03-29'
+                ));
             } else if (campsite_id == 4) {
-                events = [
-                    {
-                        title: 'Fermeture - Camping Les Tamaris',
-                        start: '2024-04-01',
-                        end: '2024-11-01',
-                        color: '#4682B4',
-                        allDay: true
-                    }
-                ];
+                events.push(generateRRuleEvent(
+                    'Fermeture - Camping Les Tamaris',
+                    '2024-04-01'
+                ));
             } else if (campsite_id == 5) {
-                events = [
-                    {
-                        title: 'Fermeture - Camping Les Flots Bleus',
-                        start: '2024-05-15',
-                        end: '2024-10-01',
-                        color: '#8A2BE2',
-                        allDay: true
-                    }
-                ];
+                events.push(generateRRuleEvent(
+                    'Fermeture - Camping Les Flots Bleus',
+                    '2024-05-15'
+                ));
             }
 
             successCallback(events);
         }
     });
+// successCallback: rendre events dans FC - render: rendre calndar sur la page 
 
     calendar.render();
 });
