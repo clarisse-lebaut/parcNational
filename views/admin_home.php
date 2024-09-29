95752b74623e8f7ff9d3c01a6b5e2ccebc4bf9bb
clarisse-lebaut <clarisse.le-baut@laplateforme.io>
2024-09-30 00:19:50 +0200

    ok... weakness for design, i cannot resit ! I start it for admin home page ^^

--- views/admin_home.php
+++ views/admin_home.php
@@ -12,140 +12,76 @@
     <header>
         <nav class="nav-admin">
             <div class="name-admin">
-                <img src="#" alt="avatar admin">
+                <img class="pic-nav-admin" src="assets/icon/users.svg" alt="avatar admin">
                 <p>Bonjour, nomdel'admin !</p>
             </div>
             <div class="icon-admin">
-                <a href="admin_home"><img src="assets/icon/home.svg" alt="icon home"></a>
-                <a href="home"><img src="assets/icon/off.svg" alt="icon off"></a>
+                <a href="admin_home"><img class="pic-nav-admin" src="assets/icon/home.svg" alt="icon home"></a>
+                <a href="home"><img class="pic-nav-admin" src="assets/icon/off.svg" alt="icon off"></a>
             </div>    
         </nav>
         <section class="data-admin">
-            <p>Adresse mail de l'admin : récupérer l'adresse mail</p>
-            <button>Modifier</button>
-        </section>
-        <section class="data-admin">
-            <p>Mot de passe : *********</p>
-            <button>Modifier</button>
+            <div>
+                <p>Adresse mail de l'admin : récupérer l'adresse mail</p>
+                <button><img src="assets/icon/edit.svg" alt="icon edit"></button>
+            </div>
+            <div>
+                <p>Mot de passe : *********</p>
+                <button><img src="assets/icon/edit.svg" alt="icon edit"></button>
+            </div>
         </section>
     </header>
     <main>
-        <section>
         <section class="data-person">
             <div>
-                <a href="manage_admin">
-                    <p>Gérer les adminstrateurs</p>
-                    <img src="assets/icon/admin.svg" alt="icon admin">
-                    <div><?php echo htmlspecialchars($total_users); ?></div>
-                </a>
+                <a href="manage_admin">Gérer les adminstrateurs</a>
+                <img src="assets/icon/admin.svg" alt="icon admin">
+                <div><?php echo htmlspecialchars($total_users); ?></div>
             </div>
             <div>
-                <!-- <a href="manage_users"> -->
-                    <p>Gérer les utilisateurs</p>
-                    <img src="assets/icon/users.svg" alt="icon user">
-                    <div><?php echo htmlspecialchars($total_users); ?></div>
-                <!-- </a> -->
+                <a href="manage_users">Gérer les utilisateurs</a>
+                <img src="assets/icon/users.svg" alt="icon user">
+                <div><?php echo htmlspecialchars($total_users); ?></div>
             </div>
+
             <div>
-                <a href="manage_visitor">
-                    <p>Gérer les visiteurs</p>
-                    <img src="assets/icon/visitor.svg" alt="icon visitors">
-                    <div>??</div>
-                </a>
-            </div>
+                <a href="manage_visitor">Gérer les visiteurs</a>
+                <img src="assets/icon/visitor.svg" alt="icon visitors">
+                <div>??</div>
+            </div>    
         </section>
 
         <section class="data-subject">
             <section class="data-page">
-                <div>
-                    <a href="manage_trails">
-                        <p>Gérer les sentiers</p>
-                        <div>
-                            <img src="assets/icon/hiking.svg" alt="icon trails">
-                            <div><?php echo htmlspecialchars($total_trails); ?></div>
-                            <div>
-                                <?php if (!empty($last_trails)): ?>
-                                    <ul>
-                                        <?php foreach ($last_trails as $trail): ?>
-                                            <li><?php echo htmlspecialchars($trail['name']); ?></li>
-                                        <?php endforeach; ?>
-                                    </ul>
-                                <?php else: ?>
-                                    <p>Aucun sentier trouvé.</p>
-                                <?php endif; ?>
-                            </div>
-                        </div>
-                    </a>
+                <div class="one">
+                    <a href="manage_trails">Gérer les sentiers</a>
+                    <img src="assets/icon/hiking.svg" alt="icon trails">
+                    <div><?php echo htmlspecialchars($total_trails); ?></div>
                 </div>
-                <div>
-                    <a href="manage_campsite">
-                        <p>Gérer les campings</p>
-                        <div>
-                            <img src="assets/icon/campsite.svg" alt="icon campsite">
-                            <div><?php echo htmlspecialchars($total_campsites); ?></div>
-                            <div>
-                                <?php if (!empty($last_campsites)): ?>
-                                    <ul>
-                                        <?php foreach ($last_campsites as $campsites): ?>
-                                            <li><?php echo htmlspecialchars($campsites['name']); ?></li>
-                                        <?php endforeach; ?>
-                                    </ul>
-                                <?php else: ?>
-                                    <p>Aucun camping trouvé.</p>
-                                <?php endif; ?>
-                            </div>
-                        </div>
-                    </a>
+   
+                <div class="two">
+                    <a href="manage_campsite">Gérer les campings</a>
+                    <img src="assets/icon/campsite.svg" alt="icon campsite">
+                    <div><?php echo htmlspecialchars($total_campsites); ?></div>
                 </div>
-                <div>
-                    <a href="manage_ressources">
-                        <p>Gérer les ressources</p>
-                        <div>
-                            <img src="assets/icon/ressources.svg" alt="icon ressources">
-                            <div><?php echo htmlspecialchars($total_ressources); ?></div>
-                            <div>
-                                <?php if (!empty($last_ressources)): ?>
-                                    <ul>
-                                        <?php foreach ($last_ressources as $ressources): ?>
-                                            <li><?php echo htmlspecialchars($ressources['name']); ?></li>
-                                        <?php endforeach; ?>
-                                    </ul>
-                                <?php else: ?>
-                                    <p>Aucune ressources trouvé.</p>
-                                <?php endif; ?>
-                            </div>
-                        </div>
-                    </a>
+
+                <div class="three">
+                    <a href="manage_ressources">Gérer les ressources</a>
+                    <img src="assets/icon/ressources.svg" alt="icon ressources">
+                    <div><?php echo htmlspecialchars($total_ressources); ?></div>
                 </div>
-                <div>
-                    <a href="manage_rapports">
-                        <p>Gérer les rapport</p>
-                        <div>
-                            <img src="assets/icon/rapport.svg" alt="icon rapport">
-                            <div><?php echo htmlspecialchars($total_rapports); ?></div>
-                            <div>
-                                <?php if (!empty($last_rapports)): ?>
-                                    <ul>
-                                        <?php foreach ($last_rapports as $rapports): ?>
-                                            <li><?php echo htmlspecialchars($rapports['name']); ?></li>
-                                        <?php endforeach; ?>
-                                    </ul>
-                                <?php else: ?>
-                                    <p>Aucun rapports trouvé.</p>
-                                <?php endif; ?>
-                            </div>
-                        </div>
-                    </a>
+
+                <div class="four">
+                    <a href="manage_rapports">Gérer les rapport</a>
+                    <img src="assets/icon/rapport.svg" alt="icon rapport">
+                    <div><?php echo htmlspecialchars($total_rapports); ?></div>
                 </div>
-            </section>
-            <section class="ship">
-                <a href="manage_ship">
-                    <div>
-                        <p>Gérer les abonnements</p>
-                        <div class="all_ship">informations</div>
-                    </div>
-                </a>
-            </section>
+
+                <div class="five">
+                    <a href="manage_ship">Gérer les abonnements</a>
+                    <div class="all_ship">informations</div>
+                </div>
+            </section>    
         </section>
     </main>
     
