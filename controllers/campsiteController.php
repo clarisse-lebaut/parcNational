752b82a90f627e46f1839087aaf57d63c2e0c1d4
maily-vannobel <maily.vannobel@laplateforme.io>
2024-10-10 13:59:50 +0200

    feat: add function (update status of campsite)

--- controllers/campsiteController.php
+++ controllers/campsiteController.php
@@ -19,12 +19,15 @@ class CampsiteController {
     public function getCampsiteById($campsite_id) {
         $campsite = $this->campsiteModel->getCampsiteById($campsite_id);
         if ($campsite) {
-            $this->render('campsiteDetails', ['campsite' => $campsite]);
+            $this->updateStatusBasedOnVacation($campsite_id, $campsite);
+            if ($campsite['status']) {
+                $this->render('campsiteDetails', ['campsite' => $campsite]);
+            }
         } else {
             return null;
         }
     }
-
+    
     // 3. Créer un nouveau camping
     public function createCampsite($name, $description, $address, $city, $zipcode, $image, $status) {
         if ($this->campsiteModel->createCampsite($name, $description, $address, $city, $zipcode, $image, $status)) {
@@ -50,5 +53,48 @@ class CampsiteController {
         } else {
             return "Erreur lors de la suppression du camping.";
         }
+    }
+
+    // 6. Mise à jour du statut ( en fonction des vacances)
+    public function updateStatusBasedOnVacation($campsite_id, $campsite) {
+        $events = $this->getVacationEvents($campsite_id);
+        $isClosed = $this->isClosedToday($events);
+    
+        $newStatus = $isClosed ? 'Fermé' : 'Ouvert';
+        
+        if ($this->campsiteModel->updateStatus($campsite_id, $newStatus)) {
+            $campsite['status'] = $newStatus;
+        }
+    }
+    
+    // 7. Obtenir les vacances d'un camping
+    public function getVacationEvents($campsite_id) {
+        $events = [];
+    
+        // Vérification de l'ID du camping et ajout des événements associés
+        if ($campsite_id == 1) {
+            $events[] = ['start' => '2024-03-25', 'end' => '2024-04-10'];
+        } else if ($campsite_id == 2) {
+            $events[] = ['start' => '2024-10-01', 'end' => '2025-04-30'];
+        } else if ($campsite_id == 3) {
+            $events[] = ['start' => '2024-03-29', 'end' => '2024-09-15'];
+        } else if ($campsite_id == 4) {
+            $events[] = ['start' => '2024-11-01', 'end' => '2025-04-20'];
+        } else if ($campsite_id == 5) {
+            $events[] = ['start' => '2024-05-15', 'end' => '2024-05-31'];
+        }
+    
+        return $events;
+    }
+        
+    // 8. Vérifier si aujourd'hui se trouve dans une période de fermeture
+    private function isClosedToday($events) {
+        $today = date('Y-m-d');
+        foreach ($events as $event) {
+            if ($today >= $event['start'] && $today <= $event['end']) {
+                return true;
+            }
+        }
+        return false;
     }
 }
