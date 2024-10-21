2b6d983edec95888a1f66e04577e30766ae89552
maily-vannobel <maily.vannobel@laplateforme.io>
2024-10-21 20:01:31 +0200

    fix adapt paymentController

--- controllers/paymentController.php
+++ controllers/paymentController.php
@@ -1,7 +1,8 @@
 <?php
 
 require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../models/ReservationModel.php';
+require_once __DIR__ . '/../models/ReservationModel.php'; 
+require_once __DIR__ . '/../models/CampsiteModel.php'; 
 
 use Stripe\Stripe;
 use Stripe\Checkout\Session;
@@ -14,14 +15,38 @@ class PaymentController {
         Stripe::setApiKey($_ENV['STRIPE_SECRET_KEY']);
     }
 
+    public function processPayment() {
+        $campsite_id = isset($_POST['campsite_id']) ? intval($_POST['campsite_id']) : 0;
+        $start_date = isset($_POST['start_date']) ? $_POST['start_date'] : '';
+        $end_date = isset($_POST['end_date']) ? $_POST['end_date'] : '';
+        $num_persons = isset($_POST['num_persons']) ? intval($_POST['num_persons']) : 0;
+        $price = isset($_POST['price']) ? floatval($_POST['price']) : 0;
+        $promo_code = isset($_POST['promo_code']) ? $_POST['promo_code'] : '';
+
+        $campsiteModel = new CampsiteModel();
+        $campsite = $campsiteModel->getCampsiteById($campsite_id);
+
+        // Vérifier s'il y a un code promo et ajuster le prix
+        if ($promo_code === 'PROMO10') {
+            $price *= 0.9;
+        }
+
+        // si 'payer' est soumis
+        if (isset($_POST['confirm_payment'])) {
+            $this->createCheckoutSession($campsite_id, $price, $start_date, $end_date, $num_persons);
+        } else {
+            require __DIR__ . '/../views/payment.php';
+        }
+    }
+
     public function createCheckoutSession($campsite_id, $price, $start_date, $end_date, $num_persons) {
         try {
             if (session_status() === PHP_SESSION_NONE) {
                 session_start();
             }
             $reservationModel = new ReservationModel();
-            $user_id = 1; 
-            $reservation_id = $reservationModel->createReservation($user_id, $campsite_id, $start_date, $end_date, $price, "en attente"); // réservation par défaut "en attente"
+            $user_id = 1; //A CHANGER USER STATIQUE 
+            $reservation_id = $reservationModel->createReservation($user_id, $campsite_id, $start_date, $end_date, $price, "en attente");
             $_SESSION['reservation_id'] = $reservation_id;
 
             $checkout_session = Session::create([
@@ -50,6 +75,7 @@ class PaymentController {
         }
     }
 
+    // reservation confirmée quand paiment reussi
     public function confirmReservation($reservation_id) {
         $reservationModel = new ReservationModel();
         return $reservationModel->updateReservationStatus($reservation_id, "confirmée");
