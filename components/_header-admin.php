4c0c9d5d91a8516f031b59b93363b3ae00828b9c
clarisse-lebaut <clarisse.le-baut@laplateforme.io>
2024-10-24 21:23:50 +0200

    sytle : correct path for admin panel, now we can navigate inside

--- components/_header-admin.php
+++ components/_header-admin.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/style/_global-admin.css">
-    <link rel="stylesheet" href="assets/style/_header-admin.css">
+    <link rel="stylesheet" href="assets/style/config/_global-admin.css">
+    <link rel="stylesheet" href="assets/style/config/_header-admin.css">
 
     <script src="assets/script/hamb.js" defer></script>
 </head>
