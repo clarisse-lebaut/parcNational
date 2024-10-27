2ec353b95d539f8cc725fb9f95231edcc72f6d03
clarisse-lebaut <clarisse.le-baut@laplateforme.io>
2024-10-28 00:01:08 +0100

    style : desing aside and article, put background and border

--- views/details_news.php
+++ views/details_news.php
@@ -4,47 +4,46 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Les Actualitées</title>
+    <link rel="stylesheet" href="assets/style/user/details_news.css">
 </head>
 <body>
     <header>
         <?php include "components/_header.php"; ?>
     </header>
-    <main>
-        <section>
-            <h1><?php echo htmlspecialchars($news['title']); ?></h1>
-        </section>
 
-        
-        <section>
-            <p><?php echo htmlspecialchars($news['published_date']); ?></p>
-        </section>
-        
-        <section>
-            <p><?php echo htmlspecialchars($news['published_time']); ?></p>
-        </section>
-        
-        <section>
-            <img src="<?php echo htmlspecialchars($news['picture']); ?>"></p>
-        </section>
+    <main class="main-container">
+        <article class="main-article">
+            <h1 class="title-news"><?php echo htmlspecialchars($news['title']); ?></h1>
 
-        <aside>
+            <section class="date_and_time">
+                <p><i>publié le : </i><?php echo htmlspecialchars($news['published_date']); ?></p>
+                <p><i>à : </i><?php echo htmlspecialchars($news['published_time']); ?></p>
+            </section>
+                
+            <section class="pic_container">
+                <img class="news-pic" src="<?php echo htmlspecialchars($news['picture']); ?>" alt="">
+            </section>
+
+            <section>
+                <p><?php echo htmlspecialchars($news['content']); ?></p>
+            </section>
+        </article>
+
+        <aside class="recent-articles">
             <h2>Articles récents</h2>
-            <ul>
+            <div>
                 <?php foreach ($allNews as $news): ?>
-                    <li>
+                    <article>
                         <a href="details_news?id=<?php echo $news['id']; ?>">
-                            <?php echo htmlspecialchars($news['title']); ?>
+                            <img src="<?php echo htmlspecialchars($news['picture']); ?>" alt="">
+                            <p><?php echo htmlspecialchars($news['title']); ?></p>
                         </a>
-                    </li>
+                    </article>
                 <?php endforeach; ?>
-            </ul>
+            </div>
         </aside>
-
-        <section>
-            <p><?php echo htmlspecialchars($news['content']); ?></p>
-        </section>
-
     </main>
+
     <footer>
         <?php include "components/_footer.php"; ?>
     </footer>
