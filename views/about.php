772ed9aa14ef1504bbf3cf80c2832640c2337f92
clarisse-lebaut <clarisse.le-baut@laplateforme.io>
2024-10-28 14:06:26 +0100

    style : fictive data in page about, just to have strucutre and desing of this page

--- views/about.php
+++ views/about.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About</title>
     <link rel="stylesheet" href="assets/style/config/_global.css">
+    <link rel="stylesheet" href="assets/style/user/about.css">
 </head>
 <body>
     <header>
@@ -13,80 +14,149 @@
 <main>
     <h1>À propos de nous</h1>
 
-        <section>
+        <section class="section">
             <h2>Notre mission</h2>
-            <p>Chez [Nom de ton site], notre mission est de [expliquer la mission, par exemple : 
-                "offrir des solutions innovantes pour améliorer la vie quotidienne de nos utilisateurs"]. 
-                Nous croyons fermement en [mentionner des valeurs fondamentales, comme "l'innovation, 
-                l'intégrité et le service client"].</p>
+            <div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, vel saepe. Saepe illum blanditiis accusantium ad corporis quis optio totam hic, similique quae magni doloremque aliquam voluptatum explicabo suscipit numquam ipsa? Doloremque, ad quibusdam. Quasi exercitationem unde quos distinctio earum nihil id vero voluptate, tempora veritatis reiciendis! Nobis, blanditiis omnis.
+            </div>
         </section>
 
-        <section>
+        <hr>
+
+        <section class="section">
             <h2>Notre vision</h2>
-            <p>Nous aspirons à [décrire la vision, par exemple : 
-                "devenir le leader dans notre secteur en proposant des solutions durables et 
-                accessibles à tous"]. Nous nous engageons à [expliquer comment vous 
-                atteindrez cette vision].</p>
+            <div>
+                <img class="pic-about" src="https://picsum.photos/500/500" alt="Image Lorem Picsum">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis iure quo ex dignissimos rem iste debitis quaerat et excepturi ad voluptatem, delectus alias fugit libero blanditiis esse? Soluta fuga molestiae, quibusdam quaerat nulla reiciendis architecto eius? Repellendus enim odit, error incidunt ducimus velit dolorum atque, reiciendis magnam perspiciatis aliquam ea? Ea hic laudantium rem necessitatibus dolorum reiciendis exercitationem dolores reprehenderit animi iste aspernatur magni ab dicta ad repudiandae pariatur repellat perspiciatis blanditiis quaerat, earum voluptas! Ut similique deserunt eum, dolor beatae, rem blanditiis praesentium quam cumque deleniti aliquid magni ipsam vero temporibus eos magnam eaque? Commodi dicta molestiae vero magni?</p>
+            </div>
         </section>
 
-        <section>
+        <hr>
+
+        <section class="section">
             <h2>Notre histoire</h2>
-            <p>Fondé en [année de création], [Nom de ton site] a été créé par 
-                [nom du fondateur ou de l'équipe] dans le but de [décrire le but de la création, 
-                par exemple : "répondre à un besoin spécifique sur le marché"]. Depuis nos débuts, 
-                nous avons [mentionner des réalisations importantes, comme "développé des produits 
-                qui ont aidé des milliers de clients"].</p>
+                <div>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum esse quos cupiditate, ratione impedit ad, cumque fuga repellendus labore nulla, laudantium pariatur amet saepe quia. Saepe delectus eveniet nesciunt laboriosam sed facere dicta blanditiis commodi, neque earum quam possimus aliquam reiciendis? Similique, numquam. Nostrum vitae quis iusto soluta illum tempora? At sit iure omnis aspernatur nam, modi tenetur aut quae, nisi accusamus necessitatibus deleniti incidunt vitae eligendi eum molestiae impedit suscipit ullam libero distinctio iusto, dolorum quis! Aperiam eligendi numquam, quidem quam a provident iure atque placeat magni non modi nihil temporibus, tenetur eius! Veritatis quisquam iure doloribus officia possimus.</p>
+                    <img class="pic-about" src="https://picsum.photos/500/500" alt="Image Lorem Picsum">
+                </div>
         </section>
 
-        <section>
+        <hr>
+
+        <section class="section">
             <h2>Nos valeurs et notre culture</h2>
-            <p>Nous valorisons [décrire les valeurs, par exemple : "la transparence, le respect et 
-                la créativité"]. Notre culture d'entreprise repose sur [expliquer la culture, par exemple : 
-                "le travail d'équipe, l'inclusion et l'innovation continue"].</p>
+            <div>
+                <img class="pic-about" src="https://picsum.photos/500/500" alt="Image Lorem Picsum">
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi iure amet consectetur. Illo accusamus repellat repellendus. Possimus vero eveniet et tenetur recusandae. Quis quos illum soluta, iure qui adipisci eius dolorem est voluptatibus laudantium suscipit, quasi vel accusamus impedit? Recusandae enim pariatur ipsam nam cum! Sequi ipsa ea perspiciatis blanditiis rem odio tempora iure? Corrupti hic optio eum impedit tempora commodi quam doloribus sit laudantium ad voluptates alias iusto, harum distinctio illo repellat, delectus magnam perferendis ea accusamus quas. Dolor rerum voluptatibus sequi itaque esse atque ratione. Ea nostrum ut voluptatum dicta cupiditate quos, velit alias, tenetur, obcaecati eum vitae?</p>
+            </div>
         </section>
 
-        <section>
+        <hr>
+
+        <section class="section">
             <h2>Notre équipe</h2>
-            <p>Notre équipe est composée de [mentionner le nombre de membres, leurs rôles et compétences]. 
-                Chacun d'entre nous apporte une expertise unique qui nous permet de [expliquer comment 
-                l'équipe contribue à la mission de l'entreprise].</p>
+            <div>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat alias quasi asperiores nostrum, ut iure.</p>
+            </div>
+            <div class="staff">
+                <div class="card_staff">
+                    <img class="pic-staff" src="assets/img/person-solid.svg" alt="img person">
+                    <p>Nom & Prénom</p>
+                    <p>Poste</p>
+                </div>
+                <div class="card_staff">
+                    <img class="pic-staff" src="assets/img/person-solid.svg" alt="img person">
+                    <p>Nom & Prénom</p>
+                    <p>Poste</p>
+                </div>
+                <div class="card_staff">
+                    <img class="pic-staff" src="assets/img/person-solid.svg" alt="img person">
+                    <p>Nom & Prénom</p>
+                    <p>Poste</p>
+                </div>
+                <div class="card_staff">
+                    <img class="pic-staff" src="assets/img/person-solid.svg" alt="img person">
+                    <p>Nom & Prénom</p>
+                    <p>Poste</p>
+                </div>
+                <div class="card_staff">
+                    <img class="pic-staff" src="assets/img/person-solid.svg" alt="img person">
+                    <p>Nom & Prénom</p>
+                    <p>Poste</p>
+                </div>
+            </div>
         </section>
 
-        <section>
+        <hr>
+
+        <section class="section">
             <h2>Ce que nous offrons</h2>
-            <p>Nous proposons [décrire les produits ou services que vous offrez, par exemple : 
-                "une large gamme de produits allant des outils technologiques aux conseils personnalisés"]. 
-                Notre objectif est de [expliquer comment vous souhaitez aider vos clients, par exemple : 
-                    "fournir des solutions adaptées à leurs besoins spécifiques"].</p>
+            <div>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos ad repellat facere deserunt unde, minima, placeat doloremque laudantium ducimus nobis ipsa ab, quasi hic iste amet nemo voluptatibus distinctio soluta. Omnis labore officiis deserunt expedita necessitatibus odio provident? Fugit, atque rerum illo, tempora modi cupiditate nisi quas quaerat optio deserunt qui voluptatibus inventore nobis cumque voluptatem officia vero? Non repellat illo eaque amet quo sunt, exercitationem fugiat. Quibusdam deserunt eveniet tempore aliquam sit totam nobis quae adipisci, aspernatur, ad nulla temporibus fuga provident, enim nostrum unde vero doloribus et autem? Veritatis, perspiciatis libero! Consequatur asperiores atque sed dignissimos quam culpa!</p>
+            </div>
         </section>
 
-        <section>
+        <hr>
+
+        <section class="section">
             <h2>Engagement envers la communauté</h2>
-            <p>Nous croyons en l'importance de redonner à notre communauté. C'est pourquoi nous soutenons 
-                [décrire les initiatives communautaires ou projets sociaux].</p>
+            <div>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum unde repudiandae adipisci distinctio, consequatur sit modi iure reprehenderit fugit, quisquam quas vitae enim, incidunt optio quam nemo? Enim, a! Quo deleniti dolorum velit illo quas officiis, voluptatem consequuntur eum incidunt vero hic maiores vitae dolore quod animi sunt nostrum doloribus doloremque quam. Consectetur nam, reprehenderit totam recusandae odio veritatis delectus quisquam? Modi nemo esse vel alias aliquid dolor quae eius, error aspernatur dolore consequatur facilis recusandae, amet facere molestiae neque porro laboriosam voluptates consectetur sequi. Quae error sunt eos dignissimos officia, vero illum eius dicta beatae suscipit distinctio? Dicta, beatae!</p>    
+            </div>
         </section>
 
-        <section>
+        <hr>
+
+        <section class="section">
             <h2>Partenaires et Collaborations</h2>
-            <p>Nous avons eu l'honneur de collaborer avec [mentionner des partenaires], ce qui nous a permis de renforcer notre expertise et d'élargir notre impact.</p>
+            <div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos enim odit praesentium architecto aliquam dolor ipsum repellendus et voluptatum eum ad dolorem voluptate labore, consequatur ducimus veniam cum esse reprehenderit. Est praesentium facere iste quia quae. Sint laudantium culpa consectetur aliquid cum iusto excepturi quo neque commodi a, non ipsam, dolores velit officia placeat impedit consequatur esse, sequi quod. Hic odit non blanditiis excepturi cumque aspernatur facere expedita fugiat et dicta a quisquam nesciunt voluptates odio repellendus, qui incidunt est? Consequuntur laboriosam vel nisi ad cumque veritatis expedita possimus placeat quod reiciendis, laborum, obcaecati eos culpa aliquid dolorum, perspiciatis quam facere impedit eius fugit? Ipsum, provident delectus officia explicabo consequuntur libero labore dolore facilis qui, molestias quia quod corrupti quasi mollitia aut perferendis dolor rerum ad incidunt eum reiciendis? Soluta reprehenderit, voluptatum deleniti minus quis explicabo! Ut error esse optio, totam explicabo rerum ipsa asperiores aliquid dolor quas odit saepe?</p>    
+            </div>
         </section>
 
-        <section>
+        <hr>
+        
+        <section class="section">
             <h2>Contactez-nous</h2>
-            <p>Nous aimerions avoir de vos nouvelles ! Si vous avez des questions ou des commentaires, 
-                n'hésitez pas à nous contacter à [adresse e-mail] ou via notre [formulaire de contact / 
-                réseaux sociaux].</p>
-        </section>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, vitae esse enim minima quisquam nihil autem quaerat consequatur ratione illo voluptate, velit odio odit ut.</p>
+            <div class="contact-form">
+                <h2>Formulaire</h2>
+                <form action="#" method="post">
+                    <div class="name-fields">
+                        <div>
+                            <label for="lastname">Nom</label>
+                            <input type="text" id="lastname" name="lastname" required>
+                        </div>
+                        <div>
+                            <label for="firstname">Prénom</label>
+                            <input type="text" id="firstname" name="firstname" required>
+                        </div>
+                    </div>
+                    <div>
+                        <label for="email">Email</label>
+                        <input type="email" id="email" name="email" required>
 
-        <section>
-            <p>Formulaire de contact</p>
-            <p>Insérer un formulaire de contact vers les admins</p>
-            <form action="">
-                
-            </form>
-        </section>
+                        <label for="message">Message</label>
+                        <textarea id="message" name="message" required></textarea>
+                    </div>
 
-        <p>Merci de visiter [Nom de ton site] et de faire partie de notre communauté !</p>
+                    <div>
+                        <button type="submit">Envoyer</button>
+                    </div>
+                </form>
+            </div>
+            <div class="social_contact">
+                <img class="pic_social" src="assets/icon/facebook.svg" alt="Icon Facebook">
+                <img class="pic_social" src="assets/icon/instagram.svg" alt="Icon Instagram">
+                <img class="pic_social" src="assets/icon/whatsapp.svg" alt="Icon Whatsapp">
+                <img class="pic_social" src="assets/icon/tiktok.svg" alt="Icon TikTok">
+                <img class="pic_social" src="assets/icon/linkedin.svg" alt="Icon Linkedin">
+            </div>
+        </section>
+        
+        <hr>
+
+        <p style="text-align:center;">Merci de visiter Parc National et de faire partie de notre communauté !</p>
     </main>
 
     <footer>
