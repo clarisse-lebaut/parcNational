0ddfbd9ca3041924bbdbe00aee9900b5e0afa94a
Pawel Barc <pawel.barc@laplateforme.io>
2024-10-30 14:16:16 +0100

    Styles added for login and register page

--- views/registerForm.php
+++ views/registerForm.php
@@ -10,34 +10,30 @@
 </head>
 
 <body>
-<header>
-		<?php include "components/_header.php"; ?>
-</header>
-
-<main>
-	<h1>Se créer un compte</h1>
-
-	<div>
-	<?php if(isset($error)): ?>
-		<div class="alert alert-danger" style="display: flex; justify-content: center; align-items: center;">
-		<?php echo htmlspecialchars($error); ?>
-		</div>
-	<?php endif; ?>
-	<?php if(isset($message)): ?>
-		<div class="message" style=" display: flex; justify-content: center; align-items: center; color: green; ">
-		<?php echo htmlspecialchars($message); ?>
-		</div>
+	<header>
+			<?php include "components/_header.php"; ?>
+	</header>
+	<main>
+		<h1>Se créer un compte</h1>
+		<?php if(isset($error)): ?>
+			<div class="alert alert-danger" style="display: flex; justify-content: center; align-items: center;">
+				<?php echo htmlspecialchars($error); ?>
+			</div>
+		<?php endif; ?>
+		<?php if(isset($message)): ?>
+			<div class="message" style=" display: flex; justify-content: center; align-items: center; color: green; ">
+				<?php echo htmlspecialchars($message); ?>
+			</div>
 		<?php endif; ?>
 		<form class="form" method="post" action="register-form" id="registerForm">
-		<input type="hidden" name="csrf_token" value="<?php echo htmlspecialchars($_SESSION['csrf_token']);?>">
+			<input type="hidden" name="csrf_token" value="<?php echo htmlspecialchars($_SESSION['csrf_token']);?>">
 			<a href="login"><p>Déjà un compte ? Connectez-vous !</p></a>
-
-			<section class="form_container">  
-
-                <section class="civil">
-					<!-- GENDER-->
+			<div class="form_container">  
+				<!-- First section conteining civil data and gender-->
+				<section class="civil">
+					<!-- Gender-->
 					<h2>Genre</h2>
-				<div class="gender">
+					<div class="gender">
 						<div>
 							<input type="radio" id="genderMale" name="gender" value="male" required>
 							<label for="genderMale">Homme</label>
@@ -50,7 +46,7 @@
 							<input type="radio" id="genderOther" name="gender" value="other" required>
 							<label for="genderOther">Autre</label>
 						</div>
-				</div>
+					</div>
 					<h2>Civilités</h2>
 					<!-- Lastname -->
 					<div>
@@ -64,82 +60,72 @@
 						<input type="text" name="firstname" id="inputFirstname" placeholder="Your firstname" required>
 					</div>
 
-				<!-- Email -->
-				<div>
-					<label for="inputEmail">E-mail</label>
-					<input type="email" name="email" id="inputEmail" placeholder="Your email" required>
-					<div id="emailError" class="error-message" style="display:none;"> L'email doit contenir '@' et être au format correct.</div>
-				</div>
-				
-				<!-- Phone -->
-				<div>
-					<label for="inputPhone">Téléphone</label>
-					<input type="tel" name="phone" id="inputPhone" placeholder="Your phone number" required>
-				</div>
-				</section>
-				
-				<section class="live">
-				<h2>Adresse</h2>
-				<!-- Address -->
-				<div>
-					<label for="inputAddress">Addresse</label>
-					<input type="text" name="adress" id="inputAddress" placeholder="1234 Main St" required>
-				</div>
-
-				<!-- City -->
-				<div>
-					<label for="inputCity">Ville</label>
-					<input type="text" name="city" id="inputCity" placeholder="City" required>
-				</div>
-
-				<!-- Zipcode -->
-				<div>
-					<label for="inputZipcode" >Code postale</label>
-					<input type="text" name="zipcode" id="inputZipcode" placeholder="Zipcode" required>
-				</div>
-				</section>
-				
-				<section class='password'>
-				<h2>Mot de passe</h2>
-				<!-- Password -->
-				<div>
-					<label for="inputPassword">Mot de passe</label>
-					<input type="password" name="password" class="form-control" id="inputPassword" placeholder="Password" required>
-					<div id="passwordError" class="error-message" style="display:none;">Le mot de passe doit contenir au moins 8 caractères, une majuscule, une minuscule, un chiffre et un caractère spécial.</div>
-				</div>
+					<!-- Email -->
+					<div>
+						<label for="inputEmail">E-mail</label>
+						<input type="email" name="email" id="inputEmail" placeholder="Your email" required>
+						<div id="emailError" class="error-message" style="display:none;"> L'email doit contenir '@' et être au format correct.</div>
+					</div>
 					
-				<!-- Repeat Password -->
-				<div>
-					<label for="inputRepeatPassword">Répétez le mot de passe</label>
-					<input type="password" name="repeatpassword" class="form-control" id="inputRepeatPassword" placeholder="Repeat password" required>          
-				</div>
-	
+					<!-- Phone -->
+					<div>
+						<label for="inputPhone">Téléphone</label>
+						<input type="tel" name="phone" id="inputPhone" placeholder="Your phone number" required>
+					</div>
+				</section>
+				<!-- Second section conteining address-->
+				<section class="live">
+					<h2>Adresse</h2>
+					<!-- Address -->
+					<div>
+						<label for="inputAddress">Addresse</label>
+						<input type="text" name="adress" id="inputAddress" placeholder="1234 Main St" required>
+					</div>
+
+					<!-- City -->
+					<div>
+						<label for="inputCity">Ville</label>
+						<input type="text" name="city" id="inputCity" placeholder="City" required>
+					</div>
+
+					<!-- Zipcode -->
+					<div>
+						<label for="inputZipcode" >Code postale</label>
+						<input type="text" name="zipcode" id="inputZipcode" placeholder="Zipcode" required>
+					</div>
+				</section>
+				<!-- Third section conteining password-->
+				<section class='password'>
+					<h2>Mot de passe</h2>
+					<!-- Password -->
+					<div>
+						<label for="inputPassword">Mot de passe</label>
+						<input type="password" name="password" class="form-control" id="inputPassword" placeholder="Password" required>
+						<div id="passwordError" class="error-message" style="display:none;">Le mot de passe doit contenir au moins 8 caractères, une majuscule, une minuscule, un chiffre et un caractère spécial.</div>
+					</div>
+						
+					<!-- Repeat Password -->
+					<div>
+						<label for="inputRepeatPassword">Répétez le mot de passe</label>
+						<input type="password" name="repeatpassword" class="form-control" id="inputRepeatPassword" placeholder="Repeat password" required>          
+					</div>
 				</section> 
-
-			</section>
-
+			</div>
 			<section class="submit_button">
-
-			<div>
-				<button type="submit">Valider</button>
-			</div>
-			
+				<div>
+					<button type="submit">Valider</button>
+				</div>
 			</section>
-
 			<div class="register-block"> 
-			<button class="register-button">
-				<a class="register-text" href="login">Connexion</a>
-				<img class="register-button-img" src="assets/icon/sign-up-icon.svg" alt="icon register">
-			</button class="register-button">
+				<button class="register-button">
+					<a class="register-text" href="login">Connexion</a>
+					<img class="register-button-img" src="assets/icon/sign-up-icon.svg" alt="icon register">
+				</button class="register-button">
 			</div>
-
 		</form>
-	</div>
-</main>
-
-<footer>
-	<?php include "components/_footer.php"; ?>
-</footer>
-
+	</main>
+	<footer>
+		<?php include "components/_footer.php"; ?>
+	</footer>
 </body>
 </html>
